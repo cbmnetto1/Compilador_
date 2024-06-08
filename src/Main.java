997de1575d949fce6db4352730e5940b227548f4
Celso Netto <celso.bmnetto1@gmail.com>
2024-06-08 15:42:01 -0300

    main incompleto

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,81 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import org.antlr.v4.runtime.*;
+import org.antlr.v4.runtime.tree.*;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import javax.swing.filechooser.FileSystemView;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Alterar filepath se for testar outro código
+        String filePath = "C:/Programação/Metodos/Compilador/CodigoProfessor.txt";
+        BufferedReader reader = new BufferedReader(new FileReader(filePath));
+        StringBuilder sb = new StringBuilder();
+        String linha;
+
+        while ((linha = reader.readLine()) != null) {
+            sb.append(linha).append("\n");
+        }
+        reader.close();
+
+        // Use ANTLR to create a lexer and parser
+        CharStream input = CharStreams.fromString(sb.toString());
+        ProgramaLexer lexer = new ProgramaLexer(input);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        ProgramaParser parser = new ProgramaParser(tokens);
+
+        // Parse the input
+        ParseTree tree = parser.programa();
+        System.out.println("Análise sintática concluída com sucesso.");
+
+        // Optional: You can use a visitor or listener to walk through the tree if needed
+        // ParseTreeWalker walker = new ParseTreeWalker();
+        // MyCustomListener listener = new MyCustomListener();
+        // walker.walk(listener, tree);
+
+        generateFiles(lexer);
+    }
+
+    private static void generateFiles(ProgramaLexer lexer) throws IOException {
+        File desktop = new File(FileSystemView.getFileSystemView().getHomeDirectory().toString());
+        File folder = new File(desktop, "Compilador");
+        createFolder(folder);
+
+        String time = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd-MM-yy HH-mm-ss"));
+
+        File tokensFile = new File(folder, "Tokens " + time + ".txt");
+        File simbolosFile = new File(folder, "Simbolos " + time + ".txt");
+        // File arvoreFile = new File(folder, "ArvoreSintatica " + time + ".txt");
+
+        writeToFile(tokensFile, lexer.getAllTokens());
+        // Simbolos would need to be captured separately if required
+    }
+
+    private static void createFolder(File folder) {
+        if (!folder.exists()) {
+            boolean created = folder.mkdirs();
+            if (created) {
+                System.out.println("Pasta criada.");
+            } else {
+                System.err.println("Não foi possível criar a pasta.");
+            }
+        } else {
+            System.out.println("A pasta já existe.");
+        }
+    }
+
+    private static void writeToFile(File file, Iterable<Token> data) throws IOException {
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        for (Token token : data) {
+            writer.write(token.toString());
+            writer.write("\n");
+        }
+        writer.close();
+    }
+
+    private static void writeToFile(File file, String data) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            writer.write(data);
         }
     }
 }
